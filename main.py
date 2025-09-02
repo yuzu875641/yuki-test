633d98e8d305b05e5633d5b06aeb7995809bd902
yuzu875641 <f.kokori85@gmail.com>
2025-09-02 18:54:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from cache import cache
 
 max_api_wait_time = 4
 max_time = 20
-apis = ['https://invidious.nikkosphere.com/', 'https://invidious.rhyshl.live/', 'https://invid-api.poketube.fun', 'https://inv.tux.pizza', 'https://pol1.iv.ggtyler.dev', 'https://yewtu.be', 'https://youtube.alt.tyil.nl/']
+apis = ['https://nyc1.iv.ggtyler.dev/', 'https://lekker.gay/', 'https://invidious.nikkosphere.com/', 'https://invidious.rhyshl.live/', 'https://invid-api.poketube.fun', 'https://inv.tux.pizza', 'https://pol1.iv.ggtyler.dev', 'https://yewtu.be', 'https://youtube.alt.tyil.nl/']
 url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
 version = "1.0"
 
